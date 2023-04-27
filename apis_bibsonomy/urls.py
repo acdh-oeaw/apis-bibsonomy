d30e68f9f4137754676e94a015f0442138d63cf6
Birger Schacht <birger.schacht@oeaw.ac.at>
2023-04-27 10:31:51 +0200

    feat: add convenience views to bibsonomy
    This commit adds a couple of convenience views to the bibsonomy module -
    namely views to list, view and delete References.
    
    It also adds a couple of useful methods to the Reference model - the
    string representation of the Reference is now concatenated from title
    pages, folio and notes.
    A bibtexjson property returns the bibtex string as dict, so we can have
    easier access to the bibtex fields. This makes it also possible to
    access those fields in templates.
    
    The commit also add templates for the various views and a `base.html`
    fallback template.

--- apis_bibsonomy/urls.py
+++ apis_bibsonomy/urls.py
@@ -1,11 +1,14 @@
+from rest_framework.routers import DefaultRouter
 from django.urls import path
-from . import api_views
+from . import api_views, views
 from . import autocompletes
 
 app_name = 'apis_bibsonomy'
 
 urlpatterns = [
     path('save_get/', api_views.SaveBibsonomyEntry.as_view(), name='savegetbibsonomyentry'),
-    path('autocomplete/', autocompletes.BibsonomyAutocomplete.as_view(), name='bibsonomyautocomplete')
+    path('autocomplete/', autocompletes.BibsonomyAutocomplete.as_view(), name='bibsonomyautocomplete'),
+    path('references/', views.ReferenceListView.as_view(), name='referencelist'),
+    path('references/<int:pk>', views.ReferenceDetailView.as_view(), name='referencedetail'),
+    path('references/<int:pk>/delete', views.ReferenceDeleteView.as_view(), name='referencedelete'),
 ]
-
