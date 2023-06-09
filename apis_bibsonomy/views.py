d6b690c09de75c99b2f6c8251be25dfb2ce2b77b
Birger Schacht <birger.schacht@oeaw.ac.at>
2023-06-09 12:19:30 +0200

    feat: implement referenceonlist view
    This commit introduces a `referenceonlist` view. This view lists all
    references on a specific model instance. It reuses the existing
    `reference_list.html` template, but splits out the actual list into a
    partial. The view by default returns the whole html page, but for
    ajax/htmx requests returns only the partial.

--- apis_bibsonomy/views.py
+++ apis_bibsonomy/views.py
@@ -1,7 +1,9 @@
+from django.contrib.contenttypes.models import ContentType
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import DeleteView
 from django.urls import reverse_lazy
+from django.http import Http404
 
 from .models import Reference
 
@@ -27,3 +29,20 @@ class ReferenceDeleteView(DeleteView):
 
 class ReferenceListView(ListView):
     model = Reference
+
+class ReferenceOnListView(ReferenceListView):
+    def get_queryset(self):
+        pk = self.kwargs.get("pk")
+        contenttype = self.kwargs.get("contenttype")
+        try:
+            contenttype = ContentType.objects.get_for_id(contenttype)
+        except ContentType.DoesNotExist:
+            raise Http404
+        return self.model.objects.filter(content_type=contenttype, object_id=pk)
+
+    def get_template_names(self):
+        # return only a partial if the request is ajax or htmx
+        partial = "HX-Request" in self.request.headers or self.request.headers.get('x-requested-with') == 'XMLHttpRequest'
+        if partial:
+            return "apis_bibsonomy/partials/reference_list.html"
+        return super().get_template_names()
