3fff43cfb56e3fa13ba94b35d4149b72fabcda13
Birger Schacht <birger.schacht@oeaw.ac.at>
2023-06-27 10:05:04 +0200

    feat(ReferenceOnListView): add form to add new reference
    This commit introduces a `ReferenceNewForm`, which is a basic form for
    References. This form is added to the ReferenceOnListView.
    The form is also included in the ReferenceOnListView template. It uses
    htmx, so the page does not have to reload if a new entry is added.
    To make this work, there is a `reinit_select2.html` partial which works
    around bugs with the dal/select2 implementation in and htmx.
    Another fix is in the `fix_select2_bootstrap_focus.html` partial: if we
    want to load a view with a select2 form via a modal, we have to override
    a function to prevent the bootstrap modal from stealing the focus.

--- apis_bibsonomy/forms.py
+++ apis_bibsonomy/forms.py
@@ -4,7 +4,7 @@ from .models import Reference
 from .autocompletes import BibsonomyAutocomplete
 from apis_core.apis_entities.fields import ListSelect2
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit
+from crispy_forms.layout import Submit, Layout, Row, Column, Div
 
 
 class ReferenceForm(ModelForm):
@@ -35,3 +35,28 @@ class ReferenceForm(ModelForm):
             self.fields['object_id'].initial = object_pk
         if attribute_name is not None:
             self.fields['attribute'].initial = attribute_name
+
+class ReferenceNewForm(ModelForm):
+    class Meta:
+        model = Reference
+        exclude = ["content_type", "object_id", "bibtex", "attribute"]
+        attrs = {'data-placeholder': 'Type to get suggestions', 'data-html': True}
+        widgets = {'bibs_url': ListSelect2(url='bibsonomy:bibsonomyautocomplete', attrs=attrs)}
+        help_texts = {
+            'folio': None,
+            'notes': None
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.layout = Layout(
+                Row(
+                    Div('bibs_url', css_class="col"),
+                ),
+                Row(
+                    Column('pages_start', css_class="col-auto col-md-2"),
+                    Column('pages_end', css_class="col-auto col-md-2"),
+                    Column('folio', css_class="col"),
+                    Column('notes', css_class="col")))
+        self.helper.add_input(Submit('submit', 'Submit', css_class='btn-primary'))
--- apis_bibsonomy/views.py
+++ apis_bibsonomy/views.py
@@ -1,11 +1,12 @@
 from django.contrib.contenttypes.models import ContentType
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import DeleteView
-from django.urls import reverse_lazy
+from django.views.generic.edit import DeleteView, FormMixin, ProcessFormView
+from django.urls import reverse_lazy, reverse
 from django.http import Http404
 
 from .models import Reference
+from .forms import ReferenceNewForm
 
 
 class ReferenceDetailView(DetailView):
@@ -30,15 +31,25 @@ class ReferenceDeleteView(DeleteView):
 class ReferenceListView(ListView):
     model = Reference
 
-class ReferenceOnListView(ReferenceListView):
-    def get_queryset(self):
-        pk = self.kwargs.get("pk")
-        contenttype = self.kwargs.get("contenttype")
+class ReferenceOnListView(ReferenceListView, FormMixin, ProcessFormView):
+    form_class = ReferenceNewForm
+
+    def dispatch(self, *args, **kwargs):
+        self.pk = self.kwargs.get("pk")
         try:
-            contenttype = ContentType.objects.get_for_id(contenttype)
+            contenttype = self.kwargs.get("contenttype")
+            self.contenttype = ContentType.objects.get_for_id(contenttype)
         except ContentType.DoesNotExist:
             raise Http404
-        return self.model.objects.filter(content_type=contenttype, object_id=pk)
+        return super().dispatch(*args, **kwargs)
+
+    def get_queryset(self):
+        return self.model.objects.filter(content_type=self.contenttype, object_id=self.pk)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["object"] = self.contenttype.get_object_for_this_type(id=self.pk)
+        return context
 
     def get_template_names(self):
         # return only a partial if the request is ajax or htmx
@@ -46,3 +57,13 @@ class ReferenceOnListView(ReferenceListView):
         if partial:
             return "apis_bibsonomy/partials/reference_list.html"
         return super().get_template_names()
+
+    def get_success_url(self):
+        return reverse('apis_bibsonomy:referenceonlist', kwargs=self.request.resolver_match.kwargs)
+
+    def form_valid(self, form):
+        args = form.cleaned_data
+        args['content_type'] = ContentType.objects.get_for_id(self.request.resolver_match.kwargs['contenttype'])
+        args['object_id'] = self.request.resolver_match.kwargs['pk']
+        ref = Reference.objects.create(**args)
+        return super().form_valid(form)
