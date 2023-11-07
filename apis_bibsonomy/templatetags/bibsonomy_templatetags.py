1b2f72ed04aa574c2895cda6ff8412bb0a530b7a
Birger Schacht <birger.schacht@oeaw.ac.at>
2023-11-07 18:11:08 +0100

    feat: add link_to_reference_on templatetag
    This commit introduces a `link_to_reference_on` template tag, which can
    be used to include link to the referenceonview from other modules. The
    tag expects a model instance as argument. It then creates a link to the
    reference on view. If the `modal` argument is passed to the templatetag,
    the created link uses htmx to fill a modal with the id #modal

--- apis_bibsonomy/templatetags/bibsonomy_templatetags.py
+++ apis_bibsonomy/templatetags/bibsonomy_templatetags.py
@@ -1,5 +1,6 @@
 from django import template
 from apis_bibsonomy.forms import ReferenceForm
+from django.contrib.contenttypes.models import ContentType
 
 register = template.Library()
 
@@ -16,3 +17,12 @@ def bibsonomy_list(content_type=None, object_pk=None, attribute_name=None):
     c_dict = {'content_type': content_type, 'object_pk': object_pk, 'attribute_name': attribute_name}
     form = ReferenceForm(**c_dict)
     return {"form": form}
+
+
+@register.inclusion_tag('apis_bibsonomy/link_to_reference_on_tag.html', takes_context=False)
+def link_to_reference_on(obj=None, modal=False):
+    return {
+            "modal": modal,
+            "object_pk": obj.pk,
+            "content_type": ContentType.objects.get_for_model(obj).id,
+    }
