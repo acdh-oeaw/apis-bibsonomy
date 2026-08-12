2c5a34d95f0af7f4ba0419a6e6d6cf7b3bf545a6
Birger Schacht <birger.schacht@oeaw.ac.at>
2026-08-12 08:26:36 +0200

    feat(management): allow to recreate all cached Zotero entries

--- apis_bibsonomy/management/commands/fetch.py
+++ apis_bibsonomy/management/commands/fetch.py
@@ -6,7 +6,17 @@ from django.conf import settings
 class Command(BaseCommand):
     help = "Fetch new Zotero entries from the Zotero API"
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--refetch",
+            action="store_true",
+            help="Delete & refetch *all* search entries.",
+        )
+
     def handle(self, *args, **options):
+        if options.get("refetch"):
+            deleted, _ = ZoteroEntry.objects.all().delete()
+            self.stdout.write(f"Deleted {deleted} cached Zotero entries")
         for c in getattr(settings, "APIS_BIBSONOMY", []):
             if c["type"] == "zotero":
                 ZoteroEntry.fetch_new(c)
