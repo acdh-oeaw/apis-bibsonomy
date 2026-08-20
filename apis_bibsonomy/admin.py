91170a0c0f37000040073c14191f7124f5178df0
Birger Schacht <birger.schacht@oeaw.ac.at>
2026-08-20 08:43:02 +0200

    feat(admin): add ZoteroEntryAdmin ModelAdmin

--- apis_bibsonomy/admin.py
+++ apis_bibsonomy/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Reference
+from .models import Reference, ZoteroEntry
 
 
 @admin.register(Reference)
 class ReferenceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ZoteroEntry)
+class ZoteroEntryAdmin(admin.ModelAdmin):
     pass
